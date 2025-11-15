7a665d703da3b52dfb959e85214e4a0bc09d8412
ICEYSELF <icey@icey.tech>
2025-11-15 16:09:22 +0800

    Remove unneeded dependency (vuetify) and use our own UI framework (?)

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,13 +1,6 @@
 import { createApp } from 'vue'
-import { createVuetify } from 'vuetify'
-import '@mdi/font/css/materialdesignicons.css'
-
-// @ts-ignore
-import 'vuetify/styles'
 
 import App from './ui/App.vue'
 import './index.css'
 
-const vuetify = createVuetify()
-
-createApp(App).use(vuetify).mount('#app')
+createApp(App).mount('#app')
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineConfig({
-  plugins: [
-    vue(transformAssetUrls),
-    vuetify({ autoImport: true })
-  ],
+  plugins: [vue()],
 })
