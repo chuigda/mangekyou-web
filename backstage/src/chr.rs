0925b83baf9ea943c30ded6780815468177ef543
Chuigda Whitegive <chuigda@doki7.club>
2025-11-20 10:45:37 +0800

    半生的遗憾谁来写，唯有过客留人间

--- backstage/src/chr.rs
+++ backstage/src/chr.rs
@@ -1,4 +1,71 @@
 use serde::{Deserialize, Serialize};
+use axum::Json;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SimulatorCHR {
+    pub universe_name: String,
+    pub literal_work_name: String,
+    pub prologue: Option<String>,
+
+    pub language: Language,
+
+    pub status_bar: StatusBarConfig,
+    pub simulator: Option<SimulatorConfig>,
+    pub memory: Option<MemorySummarizerConfig>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PlayerCHR {
+    pub player_name: String,
+    pub settings: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AdditionalCHR {
+    pub status_bar: Option<StatusBarConfig>,
+    pub simulator: Option<SimulatorConfig>,
+    pub memory: Option<MemorySummarizerConfig>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct ParseResult<T: Serialize> {
+    pub success: bool,
+    pub message: Option<String>,
+    pub data: Option<T>
+}
+
+pub async fn parse_simulator(body: String) -> Json<ParseResult<SimulatorCHR>> {
+    parse_toml(&body)
+}
+
+pub async fn parse_additional(body: String) -> Json<ParseResult<AdditionalCHR>> {
+    parse_toml(&body)
+}
+
+pub async fn parse_player(body: String) -> Json<ParseResult<PlayerCHR>> {
+    parse_toml(&body)
+}
+
+pub fn parse_toml<T>(body: &str) -> Json<ParseResult<T>>
+where
+    T: Serialize + for<'de> Deserialize<'de>,
+{
+    match toml::from_str::<T>(body) {
+        Ok(v) => Json(ParseResult {
+            success: true,
+            message: None,
+            data: Some(v),
+        }),
+        Err(e) => Json(ParseResult {
+            success: false,
+            message: Some(e.to_string()),
+            data: None,
+        }),
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Language {
@@ -34,33 +101,4 @@ pub struct StatusBarConfig {
 pub struct MemorySummarizerConfig {
     pub rules: Option<String>,
     pub sections: Option<String>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct SimulatorCHR {
-    pub universe_name: String,
-    pub literal_work_name: String,
-    pub prologue: Option<String>,
-
-    pub language: Language,
-
-    pub status_bar: StatusBarConfig,
-    pub simulator: Option<SimulatorConfig>,
-    pub memory: Option<MemorySummarizerConfig>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct AdditionalCHR {
-    pub status_bar: Option<StatusBarConfig>,
-    pub simulator: Option<SimulatorConfig>,
-    pub memory: Option<MemorySummarizerConfig>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct PlayerCHR {
-    pub player_name: String,
-    pub settings: Option<String>,
 }
--- backstage/src/main.rs
+++ backstage/src/main.rs
@@ -3,17 +3,23 @@ mod chr;
 mod llm_fwd;
 
 use axum::Router;
-use axum::routing::any;
+use axum::routing::{any, post};
 use tokio::net::TcpListener;
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-        .route("/ws", any(llm_fwd::websocket_handler));
+        .route("/ws", any(llm_fwd::websocket_handler))
+        .route("/parse/simulator", post(chr::parse_simulator))
+        .route("/parse/additional", post(chr::parse_additional))
+        .route("/parse/player", post(chr::parse_player))
+        .layer(CorsLayer::permissive());
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
 
+    tracing::info!("HTTP Server listening on http://127.0.0.1:3000");
     tracing::info!("Websocket Server listening on ws://127.0.0.1:3000/ws");
     axum::serve(listener, app).await.unwrap();
 }
